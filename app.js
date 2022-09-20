e748c710486c5c0b5714752eba2fd69a54e6a81f
ABOUBACAR ZOUNGRANA <aboubacarzoungrana@ABOUBACARs-iMac.lan>
2022-09-20 03:38:48 -0500

    add somme changes into my project

---
+++ app.js
@@ -0,0 +1,70 @@
+// set background infos 
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-modal-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button =>{
+    button.addEventListener('click', => {
+        const modal document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+
+})
+
+overlay.addEventListener('click', () => {
+    const moadls = document.querySelectorAll('.modal.active')
+    moadls.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button =>{
+    button.addEventListener('click', => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+
+})
+
+function openModal(modal) {
+    if(modal == null) return 
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if(modal == null) return 
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+//next or previous button selection
+
+
+
+
+//reset button would appear at the end of the story
+
